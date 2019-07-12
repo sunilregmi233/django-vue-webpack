2e62a4a33740be36f487ddb8f3f99dd38d83c0a4
sunilregmi233 <sunilregmi223@gmail.com>
2019-07-12 15:47:20 +0545

    django-cors-heads

--- backend/settings.py
+++ backend/settings.py
@@ -37,10 +37,18 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'webpack_loader'
+    'webpack_loader',
+    'corsheaders',
 ]
+CORS_ORIGIN_WHITELIST = (
+     "http://localhost:8080",
+)
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -126,7 +134,6 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'public')
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'dist'),
     os.path.join(BASE_DIR, 'static'),
 )
 
